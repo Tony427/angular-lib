8daceaff9ae8a4ed60a8ecb1c6ec19fe64f9bddd
TonyTsai <tony@unifiedpractice.com>
2020-11-10 14:47:12 +0800

    add dom veent object binding to variable

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
     // template的話直接使用html樣板
     // templateUrl的話則是指定獨立的html檔案樣板
     template: `
+    <input type="text" (input)="onUserInput($event)">
     <p>Hello {{ name }} !</p>
     <p>I'm the user component</p>
     `
@@ -12,4 +13,8 @@ import { Component } from '@angular/core';
 
 export class UserComponent {
     name: 'Tony';
+
+    onUserInput(event) {
+        this.name = event.target.value;
+    }
 }
