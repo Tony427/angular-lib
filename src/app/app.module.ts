f792f523685047bef050f72408efec13ad066080
TonyTsai <tony@unifiedpractice.com>
2020-11-10 15:01:05 +0800

    add 2 way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 // 新增一個user component的位置
 import { UserComponent } from './user.component';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     // module 這裡要load UserComponent，這樣App module才認識這個component
-    UserComponent
+    UserComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
     // template的話直接使用html樣板
     // templateUrl的話則是指定獨立的html檔案樣板
     template: `
-    <input type="text" (input)="onUserInput($event)">
+    <input type="text" [(ngModel)]="name">
     <p>Hello {{ name }} !</p>
     <p>I'm the user component</p>
     `
