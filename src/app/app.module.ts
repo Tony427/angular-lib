9147fdb85ca76d2e7abbf784d62c43642cf19752
TonyTsai <tony@unifiedpractice.com>
2020-11-10 16:34:27 +0800

    add comments in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,21 @@ import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
+  // 這個app module底下用了哪些Component
   declarations: [
     AppComponent,
     // module 這裡要load UserComponent，這樣App module才認識這個component
     UserComponent,
     DashboardComponent
   ],
+  // 使用了其他功能 (想像成C#的using)
   imports: [
     BrowserModule,
     FormsModule
   ],
+  // 注入那些Service ? (想像成StartUp裡的AddService?)
   providers: [],
+  // RootModule專屬
   bootstrap: [AppComponent]
 })
 export class AppModule { }
